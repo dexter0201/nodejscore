14aa2c2a54f56dce6cd5b247021d217109a7a8ff
Dexter Nguyen <lich.nguyen@amblique.com>
2016-11-06 18:25:59 +0700

    Added source for finding module

--- modules/package.js
+++ modules/package.js
@@ -1,22 +1,16 @@
 'use strict';
 
-/**
- * Module dependencies.
- */
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+module.exports = function (database) {
+    var Schema = database.connection.Schema;
 
+    var PackageSchema = new Schema({
+        name: String,
+        settings: {},
+        updated: {
+            type: Date,
+            default: Date.now
+        }
+    });
 
-/**
- * Package Schema
- */
-var PackageSchema = new Schema({
-    name: String,
-    settings: {},
-    updated: {
-    type: Date,
-        default: Date.now
-    }
-});
-
-mongoose.model('Package', PackageSchema);
+    database.connection.model('Package', PackageSchema);
+};
