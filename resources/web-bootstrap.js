bf11db4cf69ab2fa9251365a164fba30dbbf628a
Dexter Nguyen <lich.nguyen@amblique.com>
2017-04-24 23:48:23 +0700

    Added bootstrap.js

---
+++ resources/web-bootstrap.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.element(document).ready(function () {
+    // Init the app
+    angular.bootstrap(document, ['nodejscore']);
+});
+
+function processModules(modules) {
+    var packageModules = [
+        'ngCookies',
+        'ngResource',
+        'ui.bootstrap',
+        'ui.router'
+    ];
+
+    for (var index in modules) {
+        var module = modules[index],
+            moduleName = 'nodejscore.' + module.name;
+
+        angular.module(moduleName, module.angularDependencies || []);
+        packageModules.push(moduleName);
+    }
+
+    angular.module('nodejscore', packageModules);
+}
+
+JQuery.ajax('/_getModules', {
+    dataType: 'json',
+    async: false,
+    success: processModules
+});
